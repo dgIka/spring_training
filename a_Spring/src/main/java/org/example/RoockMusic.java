c20372ded57c638df7c9a147fd35029a47a583a2
Ika <rexar.ika@gmail.com>
2025-04-11 01:41:33 +0300

    day 1 Inversion of Control(IoC)

---
+++ a_Spring/src/main/java/org/example/RoockMusic.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RoockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
---
+++ a_Spring/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring  {
+    public static void main(String[] args) throws BeansException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        Music music = context.getBean("musicBean", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
